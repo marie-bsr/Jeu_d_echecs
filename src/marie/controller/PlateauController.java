2ab48e384cbd3ccdbe519b9ebf63ed2177b56933
cda <cda@cefimmac67.local>
2020-04-12 14:33:13 +0200

    affichage jeu fonctionnel

--- src/marie/controller/PlateauController.java
+++ src/marie/controller/PlateauController.java
@@ -17,7 +17,7 @@ public class PlateauController implements Initializable {
     @FXML
     private Pane pane00, pane01, pane02,pane03, pane04, pane05, pane06,pane07,
             pane10,pane11,pane12,pane13,pane14, pane15,pane16,pane17,
-            pane20, pane21, pane22, pane23, pane24, pane25, pane26, pane27,
+            pane20,pane21, pane22, pane23, pane24, pane25, pane26, pane27,
             pane30, pane31, pane32, pane33, pane34, pane35, pane36, pane37,
             pane40, pane41, pane42, pane43, pane44, pane45, pane46, pane47,
             pane50, pane51, pane52, pane53, pane54, pane55, pane56, pane57,
@@ -47,13 +47,17 @@ public class PlateauController implements Initializable {
 
     }
 
-    //methode pour mettre à jour la vue
-    //affiche le plateau avec les pieces en bonne position, cad avec images au bon endroit sur les paneaux
+    /**
+     methode pour mettre à jour la vue en fonction des infos du modele
+     affiche le plateau avec les pieces en bonne position, cad avec images au bon endroit sur les paneaux
+     si la case n'est pas vide (du modele), on ajoute une image à la case de la vue
+    */
+
     public void updateView(){
         for (int i = 0; i <8 ; i++) {
             for (int j = 0; j < 8; j++) {
                if(plateau.getPlateau()[i][j]!= null ){
-                   paneVue[i][j].getChildren().add(new ImageView( new Image(plateau.getPlateau()[i][j].getCheminImg())));
+                   paneVue[i][j].getChildren().add(new ImageView(new Image(plateau.getPlateau()[i][j].getCheminImg())));
                 }
             }
 
